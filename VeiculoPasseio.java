bda7691f34047136facd5c6a0780856fe89e19c2
JoseMD12 <josehenriquedotta61@gmail.com>
2022-03-16 18:51:49 -0300

    Classe Veiculo Passeio

--- VeiculoPasseio.java
+++ VeiculoPasseio.java
@@ -1,5 +1,20 @@
 public class VeiculoPasseio extends Veiculo{
     double consumoKmLt = 0;
     
-    public VeiculoPasseio()
+    public VeiculoPasseio(String placa, String modelo, String marca, int ano, double valor, double consumoKmLt){
+        super(placa,modelo,marca,ano,valor);
+        this.consumoKmLt = consumoKmLt;
+    }
+
+    public double getConsumoKmLt() {
+        return consumoKmLt;
+    }
+    public void setConsumoKmLt(double consumoKmLt) {
+        this.consumoKmLt = consumoKmLt;
+    }
+
+    @Override
+    public String toString(){
+        return super.toString() + ", consumo Km/L = " + consumoKmLt;
+    }
 }
