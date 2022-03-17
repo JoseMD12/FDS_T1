27b3aa5c7f9b42a6bf6ea3625431dd3aef2697db
PedroLorea <101357140+PedroLorea@users.noreply.github.com>
2022-03-17 18:03:49 -0300

    Create CatalagoVeiculos.java

---
+++ CatalagoVeiculos.java
@@ -0,0 +1,15 @@
+public Veiculo consultaPorPlaca(String placa){
+
+}
+
+public List<Veiculo> consultaPorMarca(String marca){
+    
+}
+
+public List<Veiculo>g consultaPorAno(int ano){
+    
+}
+
+public List<Veiculo> consultaPorTipo(String tipo){
+    
+}
